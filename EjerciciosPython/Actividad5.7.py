e0794ec6d2066666cdd1a3a2fe2fa814013f6358
alumno2DAM <irammo666@g.educaand.es>
2025-12-15 08:58:36 +0100

    empleado

---
+++ EjerciciosPython/Actividad5.7.py
@@ -0,0 +1,67 @@
+class Empleado:
+
+    contador_empleado=0
+
+    def __init__(self,id_empleado, nombre, edad, cargo, salario, fecha_contratacion, correo,
+                 telefono, direccion, horario, contratado, fecha_salida):
+        self.id_empleado = id_empleado
+        self.nombre = nombre
+        self.edad = edad
+        self.cargo = cargo
+        self.salario = salario
+        self.fecha_contratacion = fecha_contratacion
+        self.correo = correo
+        self.telefono = telefono
+        self.direccion = direccion
+        self.horario = horario
+        self.contratado = contratado
+        self.fecha_salida = fecha_salida
+
+        Empleado.contador_empleado+=1
+
+
+
+    def __str__(self):
+        pass
+
+
+    def despedir(self):
+        pass
+
+class Empresa:
+    def __init__(self, nombre, direccion, industria, telefono, correo, empleados):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.industria = industria
+        self.telefono = telefono
+        self.correo = correo
+        self.empleados = empleados
+
+
+    def __str__(self):
+        pass
+
+    def agregar_empleado(self):
+        pass
+
+    def eliminar_empleado(self):
+        pass
+
+    def calcular_costo_salario(self):
+        pass
+
+    def listar_empleado(self):
+        pass
+
+def principal():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    principal()
